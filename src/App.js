551c7335459b1adc2ade820f0a60cac645d26d55
germangtz <62030107+germangtz@users.noreply.github.com>
2024-08-09 12:16:27 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ExportButtons from './components/ExportButtons';
+import './styles/styles.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1>Exportación de Datos</h1>
+      <ExportButtons />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/salesData.js
@@ -0,0 +1,9 @@
+export const salesData = [
+    {id:1, name:'Enero', sales:4000},
+    {id:2, name:'Febrero', sales:3500},
+    {id:3, name:'Marzo', sales:4789},
+    {id:4, name:'Abril', sales:3890},
+    {id:5, name:'Mayo', sales:4002},
+    {id:6, name:'Junio', sales:4230},
+    {id:7, name:'Julio', sales:4220}
+];
